6ac1dfa3900672f406d3536c33fd8f81724b8016
Myro Joy Lee <myro.joy.olida.092282@gmail.com>
2023-08-16 17:52:44 -0400

    added seeds file

---
+++ server/config/seeds.js
@@ -0,0 +1,82 @@
+const db = require("./connection");
+const { User, Product, Category } = require("../models");
+const cleanDB = require("./cleanDB");
+
+db.once("open", async () => {
+  await cleanDB("Category", "categories");
+  await cleanDB("Product", "products");
+  await cleanDB("User", "users");
+
+  const categories = await Category.insertMany([
+    { name: "Scarves" },
+    { name: "Gloves" },
+    { name: "Hats" },
+    { name: "Baby" },
+    { name: "Seasonal" },
+  ]);
+
+  console.log("categories seeded");
+
+  const products = await Product.insertMany([
+    {
+      name: "Gryffindor Scarf",
+      description: `A Harry Potter-inspired swirly scarf that's sure to warm your neck on your next trip to Hogwarts!`,
+      image: "gryffindor-scarf-stripe.png",
+      category: categories[0]._id,
+      price: 29.99,
+      quantity: 3,
+    },
+    {
+      name: `S'mores Mittens`,
+      description: `A sweet pair to wear while you sip on your hot cocoa.`,
+      image: "smores-mittens.png",
+      category: categories[1]._id,
+      price: 19.99,
+      quantity: 2,
+    },
+    {
+      name: "Pumpkin Spice Hat",
+      description: `Who says pumpkin spice is only a flavor for your fall coffee drink? Match the falling leaves with this laced hat!`,
+      image: "pumpkin-spice-hat.png",
+      category: categories[2]._id,
+      price: 24.99,
+      quantity: 3,
+    },
+    {
+      name: `Infant hat & sweater set`,
+      description: `Be ready for your newborn's photo shoot with this cute ensemble!`,
+      image: "baby-hat-sweater.png",
+      category: categories[3]._id,
+      price: 39.99,
+      quantity: 3,
+    },
+    {
+      name: "Jumbo Christmas Stocking",
+      description: `Want a stocking that looks like Grandma made it? Get a customized Christmas stocking that's sure to bring in the holiday cheer year after year.`,
+      image: "christmas-stocking-snowman.png",
+      category: categories[4]._id,
+      price: 29.99,
+      quantity: 1,
+    },
+  ]);
+
+  console.log("products seeded");
+
+  console.log("All done seeding!");
+
+  await User.create({
+    firstName: "Minnie",
+    lastName: "Mouse",
+    email: "minnie@clubhouse.com",
+    password: "Minnie1234",
+    orders: [
+      {
+        products: [products[0]._id, products[0]._id, products[1]._id],
+      },
+    ],
+  });
+
+  console.log("users seeded");
+
+  process.exit();
+});
