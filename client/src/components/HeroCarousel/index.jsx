4376793c2044f44fb63fbd1ec4881be3ef882ab7
Myro Joy Lee <myro.joy.olida.092282@gmail.com>
2023-08-28 16:28:02 -0400

    personalized hero carousel

--- client/src/components/HeroCarousel/index.jsx
+++ client/src/components/HeroCarousel/index.jsx
@@ -14,25 +14,25 @@ export default function HeroCarousel() {
     <Carousel activeIndex={index} onSelect={handleSelect}>
       <Carousel.Item>
         <MittenHero text="First slide" />
-        <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        <Carousel.Caption className="carousel-header">
+          <h3>Warm up with these styles</h3>
+          {/* <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p> */}
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <YarnHero text="Second slide" />
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        <Carousel.Caption className="carousel-header">
+          <h3>Unique materials mean a unique you</h3>
+          {/* <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p> */}
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <MiscHero text="Third slide" />
-        <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>
+        <Carousel.Caption className="carousel-header">
+          <h3>Begin new traditions</h3>
+          {/* <p>
             Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
+          </p> */}
         </Carousel.Caption>
       </Carousel.Item>
     </Carousel>
